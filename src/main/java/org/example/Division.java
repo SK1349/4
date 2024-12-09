2923f3d2047c8e431a7c0585b019396ee3cedaee
SK <boo.ochevidno@gmail.com>
2024-12-09 20:41:53 +0300

    first

---
+++ src/main/java/org/example/Division.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Division {
+
+    private int id;
+
+    private String name;
+
+    public Division (int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Person {
+
+    private int id;
+
+    private  String name;
+
+    private  String gender;
+
+    private Division division;
+
+    private double salary;
+
+    private String birthDate;
+
+    public Person(int id, String name, String gender, Division division, double salary, String birthDate){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.division = division;
+        this.salary = salary;
+        this.birthDate = birthDate;
+    }
+
+
+
+
+}
